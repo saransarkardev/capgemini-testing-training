faac0f520f5a893569cb201d500bf2f412240ae7
saransarkardev <saran.sarkar.dev@gmail.com>
2026-02-25 15:48:11 +0530

    Added codes on @AfterAll Annotation and implemented it on Test Methods.

---
+++ src/test/java/org/example/AfterAllDemoTest.java
@@ -0,0 +1,56 @@
+package org.example;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// Implementation of AfterAll Annotation. (After all the Test methods....this method will run where we will use @AfterAll annotation.
+public class AfterAllDemoTest {
+
+    private static Calculator calculator;
+
+    @BeforeAll
+    static void setUpBeforeClass() {
+        calculator = new Calculator();
+        System.out.println("setUpBeforeClassMethod is calling....");
+    }
+
+    @AfterAll
+    static void tearDownAfterAll() {
+        calculator = null;
+        System.out.println("tearDownAfterAll method is calling...");
+    }
+
+    @Test
+    void addTest() {
+        int addition = calculator.add(4,2);
+        assertEquals(6, addition);
+        System.out.println("addTest method calling...");
+    }
+
+
+    @Test
+    void subtractTest() {
+        int substraction = calculator.substract(6,1);
+        assertEquals(5, substraction);
+        System.out.println("subtractTest method is calling...");
+    }
+
+
+    @Test
+    void multiplyTest() {
+        int multiplication = calculator.multiply(5,3);
+        assertEquals(15, multiplication);
+        System.out.println("multiplyTest method is calling...");
+    }
+
+
+    @Test
+    void divideTest() {
+        int division = calculator.divide(15,3);
+        assertEquals(5, division);
+        System.out.println("divideTest method is calling...");
+    }
+}
